31e9d7fcd04a4851b4493c6f249856052ea00057
Mitch Sullivan <sullivan.mitch@gmail.com>
2021-12-03 00:14:43 -0330

    Add PowerShell script for automating initial solution steps

---
+++ Start-Problem.ps1
@@ -0,0 +1,34 @@
+<#
+.Synopsis
+   Set up directory for implementing solution to a problem.
+.DESCRIPTION
+   Use the Python tools in the `tools` directory to scrape the problem statement from the Project Euler website and
+   render the implementation file templates.
+.EXAMPLE
+   .\Start-Problem.ps1 PROBLEM_NUMBER
+.INPUTS
+   PROBLEM_NUMBER
+     Valid Project Euler problem number.
+.OUTPUTS
+   Solution implementation directory with skeleton files.
+.NOTES
+   Version 1.0
+#>
+Param(
+    [Parameter(Mandatory=$true)][Int32]$PROBLEM_NUMBER
+)
+
+[Int32]$RETURN_CODE = 0
+
+Try {
+    Set-Location tools
+    pipenv run python get_problem_statement.py $PROBLEM_NUMBER
+    pipenv run python setup_problem_directory.py $PROBLEM_NUMBER
+} Catch {
+    Write-Error $Error[0]
+    $RETURN_CODE = 1
+} Finally {
+    Set-Location $PSScriptRoot
+}
+
+Exit $RETURN_CODE
