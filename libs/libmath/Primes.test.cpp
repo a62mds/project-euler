fa4865edabb458873fa5a2a8accfbac30bcb707a
Mitch Sullivan <sullivan.mitch@gmail.com>
2021-12-21 22:11:38 -0330

    Move libmath unit tests to doctest (currently failing)

--- libs/libmath/Primes.test.cpp
+++ libs/libmath/Primes.test.cpp
@@ -1,63 +1,31 @@
-#include<iostream>
-#include<cassert>
-#include<chrono>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 
 #include"Primes.h"
 
-using llint = long long int;
 
-void print_pass_fail(bool);
-
-bool test_is_prime(Primes, llint, bool);
-bool test_is_divisible_by(Primes, llint, llint, bool);
-bool test_get_smallest_prime_divisor(Primes, llint, llint);
-bool test_get_prime_factors(Primes, llint, std::vector<llint>);
-bool test_get_exponent_vector(Primes, llint, std::vector<int>);
-bool test_get_smallest_multiple(Primes, llint, llint);
-bool test_get_gcd(Primes, llint, llint);
-bool test_arith_deriv(Primes, llint, llint);
-
-int main() {
-
-	auto t0 = std::chrono::high_resolution_clock::now();
-
+TEST_CASE("Testing function Primes::is_prime") {
 	Primes pr;
 
-	std::cout << "Testing function Primes::is_prime" << std::endl;
-	print_pass_fail(test_is_prime(pr, 2, true));
-	print_pass_fail(test_is_prime(pr, 1300979, true));
-	print_pass_fail(test_is_prime(pr, 2453422, false));
+	CHECK(pr.is_prime(2));
+	CHECK(pr.is_prime(1300979));
 
-	std::cout << "Testing function Primes::is_divisble_by" << std::endl;
-	print_pass_fail(test_is_divisible_by(pr, 4, 2, true));
-	print_pass_fail(test_is_divisible_by(pr, 5, 2, false));
-	print_pass_fail(test_is_divisible_by(pr, 2453422, 2, true));
-
-	std::cout << "Testing function Primes::get_smallest_prime_divisor" << std::endl;
-	print_pass_fail(test_get_smallest_prime_divisor(pr, 2, 2));
-	print_pass_fail(test_get_smallest_prime_divisor(pr, 49, 7));
-	print_pass_fail(test_get_smallest_prime_divisor(pr, 1300979, 1300979));
-
-	auto t1 = std::chrono::high_resolution_clock::now();
-	std::cout << "Tests completed in " 
-						<< std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count() 
-						<< " ms"
-						<< std::endl;
+	CHECK(!pr.is_prime(2453422));
 }
 
-void print_pass_fail(bool test) {
-	if (test) { std::cout << "Test passed" << std::endl; }
-	else {std::cout << "Test failed***" << std::endl; }
+TEST_CASE("Testing function Primes::is_divisble_by") {
+	Primes pr;
+
+	CHECK(pr.is_divisible_by(4, 2));
+	CHECK(pr.is_divisible_by(2453422, 2));
+	
+	CHECK(!pr.is_divisible_by(5, 2));
 }
 
-bool test_is_prime(Primes pr, llint input, bool is_prime) {
-	return pr.is_prime(input) == is_prime;
-}
+TEST_CASE("Testing function Primes::get_smallest_prime_divisor") {
+	Primes pr;
 
-bool test_is_divisible_by(Primes pr, llint num, llint denom, bool is_divisible_by) {
-	return pr.is_divisible_by(num, denom) == is_divisible_by;
-}
-
-bool test_get_smallest_prime_divisor(Primes pr, llint input, llint soln) {
-	return pr.get_smallest_prime_divisor(input) == soln;
+	CHECK(pr.get_smallest_prime_divisor(2) == 2);
+	CHECK(pr.get_smallest_prime_divisor(49) == 7);
+	CHECK(pr.get_smallest_prime_divisor(1300979) == 1300979);
 }
