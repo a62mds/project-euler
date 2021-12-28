3c98eee1a08d08f5955c95dc85230cb248637ed8
Mitch Sullivan <sullivan.mitch@gmail.com>
2021-12-28 01:30:13 -0330

    Make solution to problem 1 use math library

--- problems/problem-001/problem-001/problem-001.cpp
+++ problems/problem-001/problem-001/problem-001.cpp
@@ -1,18 +1,27 @@
-#include<iostream>
-using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include <libmath/NaturalNumbers.h>
+
+
+const math::numbers::natural UPPER_BOUND = 1000;
+
 
 int main() {
 
-	int n_max{1000}; 	// upper bound
-	int sum{0};				// initialize running total
+	std::vector<math::numbers::natural> multiples_of_3 = math::get_multiples_less_than(3, UPPER_BOUND);
+	std::vector<math::numbers::natural> multiples_of_5 = math::get_multiples_less_than(5, UPPER_BOUND);
 
-	// check each num < 1000 to see if it is divisible by either 3 or 5
-	// if it is, add it to the running total
-	for (int num=1; num != n_max; num++)
-		if ( (num%3 == 0) || (num%5 == 0) )
-			sum += num;
+	std::vector<math::numbers::natural> multiples_of_3_or_5;
+	multiples_of_3_or_5.reserve(multiples_of_3.size() + multiples_of_5.size());
+	std::copy(multiples_of_3.begin(), multiples_of_3.end(), std::back_inserter(multiples_of_3_or_5));
+	// Ensure numbers that are multiples of both 3 and 5 are not counted twice
+	std::copy_if(multiples_of_5.begin(), multiples_of_5.end(), std::back_inserter(multiples_of_3_or_5), [](math::numbers::natural n) { return !math::is_divisible_by(n, 3); });
 
-	cout << "Sum = " << sum << endl;
+	math::numbers::natural sum = std::accumulate(multiples_of_3_or_5.begin(), multiples_of_3_or_5.end(), 0ull);
+	std::cout << "Sum = " << sum << std::endl;
 
 	return 0;
 }
